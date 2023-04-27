597f3a68b7b4183158cd8da87ab2da027213a024
natashabuckham <natashaisobel@gmail.com>
2023-04-27 14:25:33 +0100

    pokedex complete

--- phase_2/pokedex/pokedex.js
+++ phase_2/pokedex/pokedex.js
@@ -1,6 +1,32 @@
 const fetchPokemon = require('./api')
 
-fetchPokemon('gengar').then((response) => {console.log(response)})
+
 
 // fetchPokemon('bulbasaur')
 // fetchPokemon('charizard')
+
+class Pokedex {
+    constructor() {
+        this.pokemon = [];
+    }
+
+    catch(pokemonName) {
+        return fetchPokemon(pokemonName).then((response) => {this.pokemon.push(response)})
+    }
+
+    all() {
+        return this.pokemon;
+    }
+}
+
+// in the node REPL
+
+// const pokedex  = new Pokedex();
+// pokedex.catch('pikachu')
+
+// pokedex.catch('jigglypuff').then(() => {console.log(pokedex.all())})
+ // Adds jigglypuff to the pokedex
+// then, after some time has passed...
+ // returns an array of the pokemon in the pokedex
+
+module.exports = Pokedex
---
+++ phase_2/pokedex/pokedex.test.js
@@ -0,0 +1,23 @@
+const Pokedex = require('./pokedex')
+
+describe('Pokedex class', () => {
+    it('adds a Pokemon to the Pokedex and returns it', (done) => {
+        const pokedex = new Pokedex();
+        pokedex.catch('pikachu').then(() => {
+            expect(pokedex.all()).toEqual([{name: 'pikachu', id: 25, height: 4, weight: 60, type: ['electric']}]);
+            done();
+        });
+    });
+
+    it('adds two Pokemon to the Pokedex and returns them', (done) => {
+        const pokedex = new Pokedex();
+        pokedex.catch('pikachu');
+        pokedex.catch('jigglypuff').then(() => {
+            expect(pokedex.all()).toEqual(
+                [{name: 'pikachu', id: 25, height: 4, weight: 60, type: ['electric']},
+                {name: 'jigglypuff', id: 39, height: 5, weight: 55, type: ['normal', 'fairy']}]
+            )
+            done();
+        })
+    })
+});
